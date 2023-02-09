1d30ef6effe252c335ffa59ce94f177222fb354f
desireevharvey <desireevharvey@gmail.com>
2023-02-09 01:07:02 -0500

    styled navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './index.css';
+
 
 export default function Header() {
   return (
-    <div>
-    <Link to={"/"}>Home</Link>
-    <Link to={"/aboutme"}>About</Link>
-    <Link to={"/resume"}>Resume</Link>
-    <Link to={"/projects"}>Projects</Link>
-    </div>
+
+<Navbar bg="light" variant="light">
+<Container>
+<div class="position-absolute top-50 start-50 translate-middle">
+
+
+  <Nav className="me-auto">
+    <Nav.Link href="/">Home</Nav.Link>
+    <Nav.Link href="/aboutme">About</Nav.Link>
+    <Nav.Link href="/resume">Resume</Nav.Link>
+    <Nav.Link href="/projects">Projects</Nav.Link>
+
+  </Nav>
+  </div>
+</Container>
+</Navbar>
   )
 }
